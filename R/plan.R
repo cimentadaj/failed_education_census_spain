2f0f1205756f109fe700b6558f488ac10dd48e03
Jorge Cimentada <cimentadaj@gmail.com>
2019-11-20 17:22:32 +0100

    Runs first iteration to make sure data is read correctly

--- R/plan.R
+++ R/plan.R
@@ -1,9 +1,69 @@
-data_path <- here("data", "MicrodatosCP_NV_per_nacional_3VAR.txt")
-labels_path <- here("data", "variable_labels.xls")
+############################# Read Census 2011 data ###########################
+###############################################################################
+
+# This is the preprocessed data
+processed_path <- here("data", "2011_census.fst")
+
+# If the preprocessed data does not exist, read the raw
+# data and resave the file. The preprocessed data is just
+# the raw data saved in another format which is much faster
+# to deal with in R (because the census is too big).
+if (!file.exists(processed_path)) {
+  data_path <- here("raw_data", "MicrodatosCP_NV_per_nacional_3VAR.txt")
+
+  message("The microdatos file is being read. This can take some time but",
+          "this is only run if the preprocessed file is not saved. Once this",
+          "is run once it will not run again.")
+
+  # Read the censo
+  censo <-
+    censo2010(
+      data_path
+    )
+
+  # Save it in a new (faster) format in the processed_path
+  write_fst(censo, processed_path)
+
+}
+
+############################# Read the codebook ###############################
+###############################################################################
+
+## variables_link <- "ftp://www.ine.es/temas/censopv/cen11/Personas%20detallado_WEB.xls"
+## download.file(variables_link, destfile = "./data/variable_labels.xls")
+
+labels_path <- here("raw_data", "variable_labels.xls")
+
+variable_coding <-
+  read_xls(
+    labels_path,
+    col_names = FALSE,
+    skip = 5
+  )
+
+# Fix up variable names
+variable_coding <-
+  set_names(variable_coding, paste0("X", 1:ncol(variable_coding)))
+
+############################# Drake plan of analysis ##########################
+###############################################################################
+
+columns_to_read <- c("CPRO",
+                     "CMUN",
+                     "IDHUECO",
+                     "NORDEN",
+                     "FACTOR",
+                     "MNAC",
+                     "ANAC",
+                     "EDAD",
+                     "SEXO",
+                     "NACI")
 
 plan <-
   drake_plan(
-    read_data = target(read_census(data_path), format = "fst"),
-    read_codebook = read_codebook_labels(file_in(labels_path)),
-    process_data = target(select_few(read_data), format = "fst")
+    read_data =
+      target(
+        read_fst(file_in(processed_path), columns = columns_to_read),
+        format = "fst"
+        )
   )
